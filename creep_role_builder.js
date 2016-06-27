ed07767f1ea421274dc957b6a01bc96e9a716059
Kody J. Peterson <kodypeterson@users.noreply.github.com>
2016-06-27 14:16:46 -0400

    Create creep_role_builder.js

---
+++ creep_role_builder.js
@@ -0,0 +1,89 @@
+var cache = require('helper_cache');
+
+module.exports = function(creep) {
+    if (!creep.memory.status) {
+        creep.memory.status = 'pickup';
+    }
+
+    if (creep.memory.status === 'pickup') {
+        pickup();
+    } else {
+        building();
+    }
+
+    function pickup() {
+        if (Memory.haltBuilders) return;
+
+        var target = Game.spawns.Spawn1;
+        var result = target.transferEnergy(creep);
+        switch (result) {
+            case ERR_NOT_IN_RANGE:
+                creep.moveTo(target);
+                break;
+
+            case ERR_NOT_ENOUGH_ENERGY:
+            case ERR_FULL:
+                creep.memory.status = 'building';
+                building();
+                break;
+        }
+    }
+
+    function building() {
+        var targets = cache.get('construction_sites');
+        if (!targets) {
+            targets = updateTargets();
+        }
+
+        if (targets.length) {
+            var target = Game.getObjectById(targets[0]);
+            if (target === null) {
+                targets = updateTargets();
+                return building();
+            }
+            if (target.level !== undefined) {
+                var result = creep.upgradeController(target);
+                switch (result) {
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(target);
+                        break;
+
+                    case ERR_NOT_ENOUGH_RESOURCES:
+                        creep.memory.status = 'pickup';
+                        pickup();
+                        break;
+                }
+            } else {
+                var result = creep.build(target);
+                switch (result) {
+                    case ERR_NOT_IN_RANGE:
+                        creep.moveTo(target);
+                        break;
+
+                    case ERR_NOT_ENOUGH_RESOURCES:
+                        creep.memory.status = 'pickup';
+                        pickup();
+                        break;
+                }
+            }
+        }
+    }
+
+    function updateTargets() {
+        if (creep.room.controller.level < 2) {
+            return [creep.room.controller.id];
+        }
+        var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if (targets.length === 0) {
+            targets.push(creep.room.controller);
+        }
+        var targetIds = [];
+        targets.forEach(function(target) {
+            targetIds.push(target.id);
+        });
+        targets = targetIds;
+        cache.set('construction_sites', targetIds);
+
+        return targets;
+    }
+};
