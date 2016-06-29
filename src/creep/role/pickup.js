d804f2c672549df9caf589c443d62aef1aff7e1f
Kody Peterson <kody.peterson@disney.com>
2016-06-29 12:40:20 -0400

    cleanup

--- src/creep/role/pickup.js
+++ src/creep/role/pickup.js
@@ -70,7 +70,6 @@ module.exports = function (creep) {
     }
   }
 
-
   function getTarget() {
     var miner = Game.getObjectById(creep.memory.miner);
     var target = creep.room.lookForAt(LOOK_RESOURCES, miner);
@@ -80,80 +79,4 @@ module.exports = function (creep) {
 
     return undefined;
   }
-
-
-
-
-
-
-
-
-  //var result;
-  //if (creep.memory.unload) {
-  //  unload();
-  //} else {
-  //  load();
-  //}
-  //
-  //function load() {
-  //  var sources = cache.get('energy_resources');
-  //  if (!sources) {
-  //    sources = updateSources();
-  //  }
-  //  if (!sources[0]) return;
-  //  var target = Game.getObjectById(sources[0]);
-  //  result = creep.pickup(target);
-  //
-  //  switch (result) {
-  //    case ERR_NOT_IN_RANGE:
-  //      creep.moveTo(target);
-  //      break;
-  //
-  //    case ERR_FULL:
-  //      creep.memory.unload = true;
-  //      unload();
-  //      break;
-  //
-  //    case ERR_INVALID_TARGET:
-  //      creep.memory.unload = true;
-  //      unload();
-  //      sources = updateSources();
-  //      break;
-  //  }
-  //}
-  //
-  //function unload() {
-  //  var target = Game.getObjectById(Memory.energyDropoff);
-  //  result = creep.transfer(target, RESOURCE_ENERGY);
-  //
-  //  switch (result) {
-  //    case ERR_NOT_IN_RANGE:
-  //      creep.moveTo(target);
-  //      break;
-  //    case ERR_NOT_ENOUGH_RESOURCES:
-  //      creep.memory.unload = false;
-  //      load();
-  //      break;
-  //  }
-  //}
-  //
-  //
-  //function updateSources() {
-  //  var sources = creep.room.find(FIND_DROPPED_RESOURCES);
-  //  var dropoff = Game.getObjectById(Memory.energyDropoff);
-  //  var sourcesIds = [];
-  //  var distance = [];
-  //  var shortest = 99999999;
-  //  sources.forEach(function(source) {
-  //    var path = creep.room.findPath(dropoff.pos, source.pos);
-  //    if (path.length < shortest) {
-  //      shortest = path.length;
-  //      sourcesIds = [source.id];
-  //    }
-  //  });
-  //  sources = sourcesIds;
-  //  cache.set('energy_resources', sourcesIds);
-  //
-  //  return sources;
-  //}
 };
