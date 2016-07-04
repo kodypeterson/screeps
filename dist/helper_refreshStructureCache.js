99851d09c8f1eb8971a48b1900d7a79dd2bc5993
Kody Peterson <kodypeterson91@gmail.com>
2016-07-03 20:28:57 -0400

    bug fixes

---
+++ dist/helper_refreshStructureCache.js
@@ -0,0 +1,13 @@
+var Cache = require('helper_cache');
+
+module.exports = function() {
+    Cache.invalidate(room, 'towers');
+    Cache.invalidate(room, 'structures');
+    var structures = room.find(FIND_STRUCTURES);
+    var cacheStruct = {};
+    structures.forEach(function (structure) {
+        if (!cacheStruct[structure.structureType]) cacheStruct[structure.structureType] = [];
+        cacheStruct[structure.structureType].push(structure.id);
+    });
+    Cache.set(room, 'structures', cacheStruct, -1);
+};
