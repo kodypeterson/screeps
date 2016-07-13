31eba29c93cd8c6c89b85837e872738f59fa439a
Kody Peterson <kody.peterson@disney.com>
2016-07-13 17:51:27 -0400

    additional details for structures

--- src/helper/refreshStructureCache.js
+++ src/helper/refreshStructureCache.js
@@ -7,8 +7,37 @@ module.exports = function(room) {
     var structures = room.find(FIND_STRUCTURES);
     var cacheStruct = {};
     structures.forEach(function (structure) {
-        if (!cacheStruct[structure.structureType]) cacheStruct[structure.structureType] = [];
+        if (!cacheStruct[structure.structureType]) {
+            cacheStruct[structure.structureType] = [];
+            cacheStruct[structure.structureType + '_details'] = {};
+        }
+        if (!cacheStruct[structure.structureType + '_details'][structure.id]) {
+            cacheStruct[structure.structureType + '_details'][structure.id] = {
+
+            };
+        }
         cacheStruct[structure.structureType].push(structure.id);
+
+        var details = cacheStruct[structure.structureType + '_details'][structure.id];
+        if (structure.structureType === 'spawn') {
+            details.holding = structure.energy;
+            details.capacity = structure.energyCapacity;
+            details.store = {
+                energy: structure.energy
+            }
+        }
+        if (structure.structureType === 'extension') {
+            details.holding = structure.energy;
+            details.capacity = structure.energyCapacity;
+            details.store = {
+                energy: structure.energy
+            }
+        }
+        if (structure.structureType === 'storage') {
+            details.holding = _.sum(structure.store);
+            details.capacity = structure.storeCapacity;
+            details.store = structure.store;
+        }
     });
     Cache.set(room, 'structures', cacheStruct, -1);
 
